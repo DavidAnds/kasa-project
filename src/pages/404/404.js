bb7ba2652456ae2ce8edbb04a5362393c33a1970
DavidAnds <davidandriason7@gmail.com>
2023-06-28 16:31:52 +0200

    404 pages implemented for mobile

--- src/pages/404/404.js
+++ src/pages/404/404.js
@@ -1,7 +1,16 @@
+import styles from "./404.module.css";
+import Navbar from "../../components/navbar/navbar";
+import { Link } from "react-router-dom";
+
 function Error404() {
     return (
-        <div className="404'">
-            Ya erreur 404
+        <div className={styles.container}>
+            <Navbar/>
+            <div className={styles.content}>
+                <h1 className={styles.title}>404</h1>
+                <p className={styles.text}>Oups! La page que vous demandez n'existe pas.</p>
+                <Link to="/" className={styles.link}>Retourner sur la page d’accueil</Link>
+            </div>
         </div>
     );
 }
