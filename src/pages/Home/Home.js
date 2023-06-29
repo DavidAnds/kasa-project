43e59bd4c1cf8991833100ffb5bc880aca9d5203
DavidAnds <davidandriason7@gmail.com>
2023-06-29 12:39:31 +0200

    Home page implemented for mobile

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,22 @@
+import Thumb from "../../components/thumb/thumb";
+import data from "../../data/data.json"
+import styles from "./Home.module.css"
+
 function Home() {
+    console.log(data)
+
     return (
-        <div className="Home">
-            Home
+        <div className={styles.container}>
+            <div className={styles.title_container}>
+                <div className={styles.title_backdrop}>
+                    <h1 className={styles.title}>Chez vous, partout et ailleurs</h1>
+                </div>
+            </div>
+            <div className={styles.content}>
+                {data.map((item, index) => (
+                    <Thumb key={index} item={item}/>
+                ))}
+            </div>
         </div>
     );
 }
