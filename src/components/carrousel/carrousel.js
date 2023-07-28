20c282bbe470b3e9b09247008414f44442758d21
DavidAnds <davidandriason7@gmail.com>
2023-07-28 17:51:46 +0200

    Carrousel modification when only one images

--- src/components/carrousel/carrousel.js
+++ src/components/carrousel/carrousel.js
@@ -1,17 +1,15 @@
-import styles from './carrousel.module.css';
-import {useState, useRef, useLayoutEffect} from "react";
-import {BiChevronRight, BiChevronLeft} from "react-icons/bi";
+import styles from "./carrousel.module.css"
+import { useState, useRef, useLayoutEffect } from "react"
+import { BiChevronRight, BiChevronLeft } from "react-icons/bi"
 
-
-function Carrousel ({images}) {
-    const [currentImage, setCurrentImage] = useState(0);
-    const [widthCarrousel, setWidthCarrousel] = useState(0);
-    const ref = useRef(null);
+function Carrousel({ images }) {
+    const [currentImage, setCurrentImage] = useState(0)
+    const [widthCarrousel, setWidthCarrousel] = useState(0)
+    const ref = useRef(null)
 
     useLayoutEffect(() => {
         setWidthCarrousel(ref.current.offsetWidth)
     }, [])
-
 
     const nextImage = () => {
         setCurrentImage(currentImage < images.length - 1 ? currentImage + 1 : 0)
@@ -23,21 +21,38 @@ function Carrousel ({images}) {
 
     return (
         <div className={styles.carrousel} ref={ref}>
-            {
-                images.map((image, index) => {
-                    return (
-                        <div className={styles.carrousel_item} key={index} style={{translate: (index*widthCarrousel) - (currentImage*widthCarrousel)}}>
-                            <img src={image} className={styles.carrousel_image} alt="Housing" />
-                        </div>
-                    )
-                })
-            }
-            <button className={`${styles.carrousel_btn} ${styles.carrousel_next}`} onClick={nextImage} >
-                <BiChevronRight className={styles.carrousel_icon}/>
-            </button>
-            <button className={`${styles.carrousel_btn}`} onClick={prevImage} >
-                <BiChevronLeft className={styles.carrousel_icon}/>
-            </button>
+            {images.map((image, index) => {
+                return (
+                    <div
+                        className={styles.carrousel_item}
+                        key={index}
+                        style={{
+                            translate:
+                                index * widthCarrousel -
+                                currentImage * widthCarrousel,
+                        }}>
+                        <img
+                            src={image}
+                            className={styles.carrousel_image}
+                            alt="Housing"
+                        />
+                    </div>
+                )
+            })}
+            {images.length > 2 && (
+                <>
+                    <button
+                        className={`${styles.carrousel_btn} ${styles.carrousel_next}`}
+                        onClick={nextImage}>
+                        <BiChevronRight className={styles.carrousel_icon} />
+                    </button>
+                    <button
+                        className={`${styles.carrousel_btn}`}
+                        onClick={prevImage}>
+                        <BiChevronLeft className={styles.carrousel_icon} />
+                    </button>
+                </>
+            )}
         </div>
     )
 }
